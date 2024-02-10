3a44fd14462235638fa3ecf4285214f489802dec
chelshuffman <72845247+chelshuffman@users.noreply.github.com>
2024-02-09 23:55:25 -0800

    Update main.js

--- Homework2/chuffman/main.js
+++ Homework2/chuffman/main.js
@@ -30,7 +30,6 @@ d3.csv("./data/ds_salaries.csv").then(rawData =>{
         d.salary_in_usd = Number(d.salary_in_usd);
         d.remote_ratio = Number(d.remote_ratio);
         d.company_size = String(d.company_size);
-        console.log(d.company_size);
     });
 
     const svg = d3.select("svg");
@@ -235,6 +234,76 @@ d3.csv("./data/ds_salaries.csv").then(rawData =>{
       .attr("font-size", "50px")
       .attr('text-anchor', 'middle')
       .text('Red = entry/mid level roles; Blue = senior/executive level roles');
+
+      // last one
+
+    const g5 = svg.append("g")
+      .attr("width", teamWidth + teamMargin.left + teamMargin.right)
+      .attr("height", teamHeight + teamMargin.top + teamMargin.bottom)
+      .attr("transform", `translate(0, 1500)`);
+
+      var y = d3.scaleLinear()
+    .domain([ 0,460000 ])          // Note that here the Y scale is set manually
+    .range([1500, 0])
+  g5.append("g").call( d3.axisLeft(y) )
+
+  // Build and Show the X scale. It is a band scale like for a boxplot: each group has an dedicated RANGE on the axis. This range has a length of x.bandwidth
+  var x = d3.scaleBand()
+    .range([ 0, width ])
+    .domain(["S", "M", "L"])
+    .padding(0.05)     // This is important: it is the space between 2 groups. 0 means no padding. 1 is the maximum.
+  g5.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+
+  // Features of the histogram
+  var histogram = d3.histogram()
+        .domain(y.domain())
+        .thresholds(y.ticks(20))    // Important: how many bins approx are going to be made? It is the 'resolution' of the violin plot
+        .value(d => d)
+
+  // Compute the binning for each group of the dataset
+  var sumstat = d3.nest()  // nest function allows to group the calculation per level of a factor
+    .key(function(d) { return d.company_size;})
+    .rollup(function(d) {   // For each key..
+      input = d.map(function(g) { return g.salary_in_usd;})    // Keep the variable called Sepal_Length
+      bins = histogram(input)   // And compute the binning on it.
+      return(bins)
+    })
+    .entries(rawData)
+
+  // What is the biggest number of value in a bin? We need it cause this value will have a width of 100% of the bandwidth.
+  var maxNum = 0
+  for ( i in sumstat ){
+    allBins = sumstat[i].value
+    lengths = allBins.map(function(a){return a.length;})
+    longuest = d3.max(lengths)
+    if (longuest > maxNum) { maxNum = longuest }
+  }
+
+  // The maximum width of a violin must be x.bandwidth = the width dedicated to a group
+  var xNum = d3.scaleLinear()
+    .range([0, x.bandwidth()])
+    .domain([-maxNum,maxNum])
+
+  // Add the shape to this svg!
+  g5
+    .selectAll("myViolin")
+    .data(sumstat)
+    .enter()        // So now we are working group per group
+    .append("g")
+      .attr("transform", function(d){ return("translate(" + x(d.key) +" ,0)") } ) // Translation on the right to be at the group position
+    .append("path")
+        .datum(function(d){ return(d.value)})     // So now we are working bin per bin
+        .style("stroke", "none")
+        .style("fill","#69b3a2")
+        .attr("d", d3.area()
+            .x0(function(d){ return(xNum(-d.length)) } )
+            .x1(function(d){ return(xNum(d.length)) } )
+            .y(function(d){ return(y(d.x0)) } )
+            .curve(d3.curveCatmullRom)    // This makes the line smoother to give the violin appearance. Try d3.curveStep to see the difference
+        );
+
     
 
 
